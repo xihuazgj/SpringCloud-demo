4d0d22183ba9b3293b1e408c98969e8c639b9f0a
guojunzhang <1965572081@qq.com>
2024-07-12 19:36:25 +0800

    微服务项目基本拆分完成

---
+++ pay-service/src/main/java/com/hmall/pay/PayApplication.java
@@ -0,0 +1,15 @@
+package com.hmall.pay;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients(basePackages = "com.hmall.api.client")
+@MapperScan("com.hmall.pay.mapper")
+@SpringBootApplication
+public class PayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+}
